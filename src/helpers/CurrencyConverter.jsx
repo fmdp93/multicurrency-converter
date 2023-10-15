40317c7931b3717e502ef37fe2dd3c003d062ba4
Francis <fmdp93@gmail.com>
2023-10-15 13:31:13 +0800

    removed target currency temporarily

--- src/helpers/CurrencyConverter.jsx
+++ src/helpers/CurrencyConverter.jsx
@@ -1,15 +1,16 @@
 export class CurrencyConverter {
     baseCurrency = "EUR";
 
-    constructor(currency, amount, rate, targetRate, targetCurrency) {
+    constructor(currency, amount, rate, targetRate) {
         this.currency = currency;
         this.amount = amount;
         this.rate = rate;
         this.targetRate = targetRate;
-        this.targetCurrency = targetCurrency;
     }
     baseCurrencyToTargetCurrency() {
         if (this.currency === this.baseCurrency)
             return this.amount * this.targetRate;
     }
+
+
 }
