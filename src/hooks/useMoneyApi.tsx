520db251a0adee0fa89f5aa40375ede53e4ef94c
Francis <fmdp93@gmail.com>
2023-11-07 15:45:12 +0800

    update typing

--- src/hooks/useMoneyApi.tsx
+++ src/hooks/useMoneyApi.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { ratesToday } from "../../db/money";
 
-const useMoneyApi = (setRates) => {
+const useMoneyApi = (setRates: CallableFunction) => {
     useEffect(() => {
         // const API_KEY = "d96099fee475a7c4c256c09f86e27a8d";
         // fetch(uri)
