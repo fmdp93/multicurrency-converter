3dbac71f621a6ebd50de0010d2cc13a38807562d
Francis <fmdp93@gmail.com>
2023-11-07 15:45:16 +0800

    update typing

--- src/components/ConverterContext.tsx
+++ src/components/ConverterContext.tsx
@@ -1,25 +1,35 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect, Dispatch, SetStateAction } from "react";
 
-export const CtxConverter = createContext(null);
+
+type ConvertContextValue = {
+    rates: number | null,
+    baseCurrency: string,
+    setBaseCurrency: Dispatch<SetStateAction<string>>,
+    baseAmount: string,
+    setBaseAmount: Dispatch<SetStateAction<string>>,
+}
+
+export const CtxConverter = createContext<ConvertContextValue | null>(null);
 
 const ConverterContext = (props: any) => {
     useContext(CtxConverter);
-    const [baseCurrency, setBaseCurrency] = useState(props.currency);
-    const [baseAmount, setBaseAmount] = useState("");
+    const [baseCurrency, setBaseCurrency] = useState<string>(props.currency);
+    const [baseAmount, setBaseAmount] = useState<string>("");
 
+    const contextVal: ConvertContextValue = {
+        rates: props.rates,
+        baseCurrency,
+        setBaseCurrency,
+        baseAmount,
+        setBaseAmount,
+    };
     useEffect(()=>{
-        console.log(props.rates);
+        // console.log(props.rates);
     },[baseAmount, baseCurrency]);
 
     return (
         <CtxConverter.Provider
-            value={{
-                rates: props.rates,
-                baseCurrency,
-                setBaseCurrency,
-                baseAmount,
-                setBaseAmount,
-            }}
+            value={contextVal}
         >
             {props.children}
         </CtxConverter.Provider>
