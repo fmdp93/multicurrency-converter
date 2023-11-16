0b4fff66f9f089f50725387ea00140c772e9227e
Francis <fmdp93@gmail.com>
2023-11-16 14:35:04 +0800

    removed moneyIsValid2

--- src/helpers/validation.tsx
+++ src/helpers/validation.tsx
@@ -4,12 +4,3 @@ export function moneyIsValid(number: string) {
         return true;
     }
 }
-
-export function moneyIsValid2(number: string) {
-    if (number.match(/^(0*[!0]\d*)?\.[\d]*$/gm)) {
-        return {
-            status: false,
-            code: CODE_PREP,
-        };
-    }
-}
